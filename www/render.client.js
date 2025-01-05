d8a2d7175711afacff2657725b1d94c29ab01e26
Razvan <razvan@razshare.dev>
2025-01-05 09:18:09 +0100

    chore(app): updating to v0.9.1.

--- www/render.client.js
+++ www/render.client.js
@@ -1,6 +1,4 @@
 import { hydrate } from "svelte";
 import main from "./main.svelte";
 // @ts-ignore
-const target = document.getElementById("app");
-let props = JSON.parse(atob(target.dataset.props));
-hydrate(main, { target, props });
+hydrate(main, { target: target(), props: props() });

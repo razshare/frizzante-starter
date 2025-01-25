08639d1aeb89e2cc6fdc798bf376a818aeaff266
Razvan <razvan@razshare.dev>
2025-01-25 18:58:42 +0100

    chore(app): removing unused about page.

--- prepare/main.go
+++ prepare/main.go
@@ -6,6 +6,5 @@ func main() {
 	frz.SveltePrepareStart()
 	frz.SveltePreparePage("welcome", "./www/lib/pages/welcome.svelte")
 	frz.SveltePreparePage("todo", "./www/lib/pages/todo.svelte")
-	frz.SveltePreparePage("about", "./www/lib/pages/about.svelte")
 	frz.SveltePrepareEnd()
 }

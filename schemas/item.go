2fee1d60e705b744f53fbe4247df8e48e82b4984
Razvan <razvan@razshare.dev>
2025-02-03 01:38:18 +0100

    chore(app): updating schemas.

--- schemas/item.go
+++ schemas/item.go
@@ -1,6 +1,6 @@
 package schemas
 
 type Item struct {
-	Checked     bool   `json:"checked" sql:"bit not null default 0"`
-	Description string `json:"description" sql:"varchar(255) not null default ''"`
+	Checked     bool   `json:"checked"`
+	Description string `json:"description"`
 }

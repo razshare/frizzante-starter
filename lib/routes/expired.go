a4a0fed438049d1e94d1121e3693d7f7d618652c
Razvan <razvan@razshare.dev>
2025-05-27 12:46:05 +0200

    chore(app): bump.

--- lib/routes/expired.go
+++ lib/routes/expired.go
@@ -1,8 +1,6 @@
 package routes
 
-import (
-	"github.com/razshare/frizzante"
-)
+import "github.com/razshare/frizzante"
 
 func GetExpired(req *frizzante.Request, res *frizzante.Response) {
 	res.SendView(frizzante.View{

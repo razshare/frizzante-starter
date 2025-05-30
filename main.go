dff3eeebc72f18e38d648454845f37a19e8960a8
Razvan <razvan@razshare.dev>
2025-05-30 22:15:42 +0200

    chore(app): bump.

--- main.go
+++ main.go
@@ -9,17 +9,17 @@ import (
 
 //go:embed .dist/*/**
 var dist embed.FS
-var server = frizzante.
-	NewServer().
-	WithNotifier(notifiers.Console).
-	WithAddress("127.0.0.1:8080").
-	WithRequestHandler("GET /events", routes.GetEvents).
-	WithRequestHandler("GET /expired", routes.GetExpired).
-	WithRequestHandler("GET /todos", routes.GetTodos).
-	WithRequestHandler("POST /todos", routes.PostTodos).
-	WithRequestHandler("GET /welcome", routes.GetWelcome).
-	WithRequestHandler("GET /", routes.GetDefault)
 
 func main() {
-	server.Start(dist)
+	frizzante.
+		NewServer().
+		WithNotifier(notifiers.Console).
+		WithAddress("127.0.0.1:8080").
+		WithRequestHandler("GET /events", routes.GetEvents).
+		WithRequestHandler("GET /expired", routes.GetExpired).
+		WithRequestHandler("GET /todos", routes.GetTodos).
+		WithRequestHandler("POST /todos", routes.PostTodos).
+		WithRequestHandler("GET /welcome", routes.GetWelcome).
+		WithRequestHandler("GET /", routes.GetDefault).
+		Start(dist)
 }

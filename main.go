812fc28eeb05c1fce589cac0502b0045cc99acc2
Razvan <razvan@razshare.dev>
2025-05-31 04:35:05 +0200

    chore(app): bump.

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"embed"
 	"github.com/razshare/frizzante"
+	"main/lib/handlers"
 )
 
 //go:embed .dist/*/**
 var dist embed.FS
 
 func main() {
-	frizzante.NewServer().WithDist(dist).Start()
+	frizzante.NewServer().
+		WithDist(dist).
+		WithRequestHandler("GET /", handlers.GetDefault).
+		WithRequestHandler("GET /welcome", handlers.GetWelcome).
+		WithRequestHandler("GET /todos", handlers.GetTodos).
+		WithRequestHandler("POST /todos", handlers.PostTodos).
+		Start()
 }

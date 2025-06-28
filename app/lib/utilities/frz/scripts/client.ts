bc58c2517d022d17e08afa87aea332e34cc8e8e4
Razvan <razvan@razshare.dev>
2025-06-28 18:22:49 +0200

    chore(.air.toml): including make check in cmd.

--- app/lib/utilities/frz/scripts/client.ts
+++ app/lib/utilities/frz/scripts/client.ts
@@ -2,7 +2,4 @@ import { hydrate } from "svelte"
 import ClientView from "$lib/utilities/frz/components/ClientView.svelte"
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
-target().innerHTML = ""
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 hydrate(ClientView, { target: target(), props: props() })

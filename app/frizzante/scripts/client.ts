add90fd497c753166ff6eed66deee457e6c5a04c
Razvan <razvan@razshare.dev>
2025-06-29 09:31:21 +0200

    chore(app): tagging version v1.2.10

--- app/frizzante/scripts/client.ts
+++ app/frizzante/scripts/client.ts
@@ -2,4 +2,7 @@ import { hydrate } from "svelte"
 import ClientView from "$frizzante/components/ClientView.svelte"
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
+target().innerHTML = ""
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 hydrate(ClientView, { target: target(), props: props() })

08bf805f17e11db7d67cbe99b366a539f3c71c0d
Razvan <razvan@razshare.dev>
2025-07-30 08:44:01 +0200

    chore(app): adding dotenv management.

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/razshare/frizzante v1.14.39
 require (
 	github.com/evanw/esbuild v0.25.8 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	rogchap.com/v8go v0.9.0 // indirect
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"embed"
+	"github.com/joho/godotenv"
 	"main/lib/handlers"
+	"os"
 
 	"github.com/razshare/frizzante/routes"
 	"github.com/razshare/frizzante/servers"
@@ -13,6 +15,19 @@ var efs embed.FS
 var server = servers.New()
 
 func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		server.Notifier.SendError(err)
+	} else {
+		server.Address = os.Getenv("server.address")
+		server.SecureAddress = os.Getenv("server.secure_address")
+		server.Key = os.Getenv("server.key")
+		server.Certificate = os.Getenv("server.certificate")
+		server.ViewRoot = os.Getenv("server.view_root")
+		server.PublicRoot = os.Getenv("server.public_root")
+		server.ViewServer = os.Getenv("server.view_server")
+		server.ViewIndex = os.Getenv("server.view_index")
+	}
+
 	server.Efs = efs
 	server.AddRoute(routes.Route{Pattern: "GET /", Handler: handlers.Default})
 	server.AddRoute(routes.Route{Pattern: "GET /welcome", Handler: handlers.Welcome})

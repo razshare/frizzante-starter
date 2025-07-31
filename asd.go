ce7f499e8c51bb89c03de13ad1f23dfbca08b316
Razvan <razvan@razshare.dev>
2025-08-01 00:55:14 +0200

    chore(app): bump.

---
+++ asd.go
@@ -0,0 +1,6 @@
+package main
+
+func init() {
+	var items = make([]string, 0)
+	items = append(items)
+}
--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"embed"
-	"github.com/razshare/frizzante/environments"
 	"github.com/razshare/frizzante/routes"
 	"github.com/razshare/frizzante/servers"
-	"github.com/razshare/frizzante/traces"
 	"main/lib/routes/handlers"
-	"os"
 )
 
 //go:embed app/dist
@@ -16,19 +13,6 @@ var server = servers.New()
 
 func main() {
 	server.Efs = efs
-
-	if err := environments.LoadDotenv(".env"); err != nil {
-		traces.Trace(server.ErrorLog, err)
-	} else {
-		server.Address = os.Getenv("server.address")
-		server.SecureAddress = os.Getenv("server.secure_address")
-		server.Key = os.Getenv("server.key")
-		server.Certificate = os.Getenv("server.certificate")
-		server.PublicRoot = os.Getenv("server.public_root")
-		server.AppRoot = os.Getenv("server.app_root")
-		server.ServerJs = os.Getenv("server.server_js")
-		server.IndexHtml = os.Getenv("server.index_html")
-	}
 
 	server.Routes = []routes.Route{
 		{Pattern: "GET /", Handler: handlers.Default},
